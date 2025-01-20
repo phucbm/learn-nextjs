edafe692f80058824499b2e34f6c5743ac87c150
phucbm <hiphucbui@gmail.com>
2025-01-20 14:44:43 +0700

    feat(auth): show username after logged in

--- nextjs-dashboard/app/ui/dashboard/sidenav.tsx
+++ nextjs-dashboard/app/ui/dashboard/sidenav.tsx
@@ -2,9 +2,12 @@ import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import {PowerIcon} from '@heroicons/react/24/outline';
-import {signOut} from "@/auth";
+import {signOut, auth} from "@/auth";
 
-export default function SideNav() {
+export default async function SideNav() {
+    const session = await auth();
+    const username = session?.user?.name || session?.user?.email;
+
     return (
         <div className="flex h-full flex-col px-3 py-4 md:px-2">
             <Link
@@ -27,7 +30,10 @@ export default function SideNav() {
                     <button
                         className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
                         <PowerIcon className="w-6"/>
-                        <div className="hidden md:block">Sign Out</div>
+                        <div className="hidden md:block">
+                            <span className="mr-2">{username}</span>
+                            Sign Out
+                        </div>
                     </button>
                 </form>
             </div>
