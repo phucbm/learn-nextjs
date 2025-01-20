612b1c1fddffb7695791198f546aaf2499466a60
phucbm <hiphucbui@gmail.com>
2025-01-20 14:35:16 +0700

    feat(chap16): adding metadata

--- nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,19 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import {fetchCustomers, fetchInvoiceById} from '@/app/lib/data';
 import {notFound} from "next/navigation";
+import {Metadata} from "next";
+
+// Add generateMetadata function
+export async function generateMetadata(
+    props: { params: Promise<{ id: string }> }
+): Promise<Metadata> {
+    const params = await props.params;
+    const id = params.id;
+
+    return {
+        title: `Edit Invoice ${id}`,
+    };
+}
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
     const params = await props.params;
@@ -18,7 +31,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
         <main>
             <Breadcrumbs
                 breadcrumbs={[
-                    { label: 'Invoices', href: '/dashboard/invoices' },
+                    {label: 'Invoices', href: '/dashboard/invoices'},
                     {
                         label: 'Edit Invoice',
                         href: `/dashboard/invoices/${id}/edit`,
@@ -26,7 +39,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
                     },
                 ]}
             />
-            <Form invoice={invoice} customers={customers} />
+            <Form invoice={invoice} customers={customers}/>
         </main>
     );
 }
