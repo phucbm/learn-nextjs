954e72174b0bc927cb53a87e264f7cbb037ae968
phucbm <hiphucbui@gmail.com>
2025-01-22 11:52:03 +0700

    feat(auth): enable Credentials sign in

--- nextjs-dashboard/auth.ts
+++ nextjs-dashboard/auth.ts
@@ -1,8 +1,33 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 import {authConfig} from "@/auth.config";
+import Credentials from "next-auth/providers/credentials";
+import {getUser} from "@/app/lib/auth/get-user-by-email";
+import {z} from "zod";
+import bcrypt from "bcryptjs";
 
 export const {handlers, signIn, signOut, auth} = NextAuth({
     ...authConfig,
-    providers: [GitHub],
+    providers: [
+        GitHub,
+        Credentials({
+            async authorize(credentials) {
+                const parsedCredentials = z
+                    .object({email: z.string().email(), password: z.string().min(6)})
+                    .safeParse(credentials);
+
+                if (parsedCredentials.success) {
+                    const {email, password} = parsedCredentials.data;
+                    const user = await getUser(email);
+                    if (!user) return null;
+                    const passwordsMatch = await bcrypt.compare(password, user.password);
+
+                    if (passwordsMatch) return user;
+                }
+
+                console.log('Invalid credentials');
+                return null;
+            },
+        }),
+    ],
 })
